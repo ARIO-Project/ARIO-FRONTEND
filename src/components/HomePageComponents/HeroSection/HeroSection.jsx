d1b226c4a4b5cee0043a732d1fea93e2b887ddb4
Daniel Fasiku <adewolefas@gmail.com>
2024-11-15 01:39:51 +0100

    IMplemented fast image load optimization

--- src/components/HomePageComponents/HeroSection/HeroSection.jsx
+++ src/components/HomePageComponents/HeroSection/HeroSection.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import useBackgroundImageOptimized from '../../../Hooks/useBackgroundImageOptimized'
+import BackgroundImage1 from '../../../assets/hero-pic1.png';
+import BackgroundImage2 from '../../../assets/hero-pic-2.png';
 import './HeroSection.css'
 
 const HeroSection = () => {
+    const [elementRef, isLoaded] = useBackgroundImageOptimized(BackgroundImage1, BackgroundImage2)
+
     return (
-        <header className='hero-section-container'>
-            <div className="hero-section-wrapper">
+        <header ref={elementRef} style={isLoaded ? { backgroundImage: `url(${BackgroundImage1})`} : {}} className='hero-section-container'  >
+            <div ref={elementRef} style={isLoaded ? { backgroundImage: `url(${BackgroundImage2})`} : {}} className="hero-section-wrapper">
                 <h1 className="hero-section-text1">SEW AND GET IN <span className="hero-section-text1-span">3</span> DAYS</h1>
                 <h4 className="hero-section-text2">Experience the Art of Nigerian Traditional Wear</h4>
                 <p className="hero-section-text3">Custom-made outfits tailored to your style and measurements.</p>
