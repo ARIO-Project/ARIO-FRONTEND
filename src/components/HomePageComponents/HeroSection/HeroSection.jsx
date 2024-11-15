ab358e318b78c878556cdaf50320f21bc8ec8aa9
Daniel Fasiku <adewolefas@gmail.com>
2024-11-15 08:57:42 +0100

    minor

--- src/components/HomePageComponents/HeroSection/HeroSection.jsx
+++ src/components/HomePageComponents/HeroSection/HeroSection.jsx
@@ -8,12 +8,16 @@ const HeroSection = () => {
     const [elementRef, isLoaded] = useBackgroundImageOptimized(BackgroundImage1, BackgroundImage2)
 
     return (
-        <header ref={elementRef} style={isLoaded ? { backgroundImage: `url(${BackgroundImage1})`} : {}} className='hero-section-container'  >
-            <div ref={elementRef} style={isLoaded ? { backgroundImage: `url(${BackgroundImage2})`} : {}} className="hero-section-wrapper">
-                <h1 className="hero-section-text1">SEW AND GET IN <span className="hero-section-text1-span">3</span> DAYS</h1>
-                <h4 className="hero-section-text2">Experience the Art of Nigerian Traditional Wear</h4>
-                <p className="hero-section-text3">Custom-made outfits tailored to your style and measurements.</p>
-                <button className="hero-section-button">Get Started</button>
+        <header className='hero-section-container'  >
+            <img src={BackgroundImage1} alt="hero-section" className='hero-section-image1' />
+            <div className="hero-section-wrapper">
+                <img src={BackgroundImage2} alt="hero-section" className='hero-section-image2' />
+                <div className="hero-section-text-wrapper">
+                    <h1 className="hero-section-text1">SEW AND GET IN <span className="hero-section-text1-span">3</span> DAYS</h1>
+                    <h4 className="hero-section-text2">Experience the Art of Nigerian Traditional Wear</h4>
+                    <p className="hero-section-text3">Custom-made outfits tailored to your style and measurements.</p>
+                    <button className="hero-section-button">Get Started</button>
+                </div>
             </div>
         </header>
     )
